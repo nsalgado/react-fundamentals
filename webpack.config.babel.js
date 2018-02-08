391eb1ad5844fa37d3211438825c831d6940ffa9
Salgado <nunos@invine.com>
2018-02-08 14:07:37 +0000

    config firebase to host battle app

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,8 +1,9 @@
 import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import webpack from 'webpack'
 
 
-module.exports = {
+let config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -21,4 +22,18 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin({
     template: './src/index.html'
   })]
+};
+
+
+if (process.env.NODE_ENV === "production") {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
 }
+
+export default config;
